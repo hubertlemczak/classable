915d6a4ff918254890560e224457e15adeacdaae
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-17 18:05:19 +0100

    build: client api

---
+++ client/src/client/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const host = 'http://localhost:4040/api';
+
+const client = {
+  get: async path => {
+    const token = localStorage.getItem('chat-app-token');
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return axios.get(`${host}${path}`, { headers });
+  },
+  post: async (path, data) => {
+    const token = localStorage.getItem('chat-app-token');
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return axios.post(`${host}${path}`, data, { headers });
+  },
+  patch: async (path, data) => {
+    const token = localStorage.getItem('chat-app-token');
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return axios.patch(`${host}${path}`, data, { headers });
+  },
+  delete: async path => {
+    const token = localStorage.getItem('chat-app-token');
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return axios.delete(`${host}${path}`, { headers });
+  },
+};
+
+export default client;
