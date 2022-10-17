7cfc688e0889360956b4ab2cf84e57196026d391
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-17 14:30:15 +0100

    feat: auth middleware

---
+++ server/src/auth/index.ts
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+import { HttpException } from '../api/errors';
+import dbClient from '../utils/dbClient';
+import { TDecodedUser, TRequestWithUser } from '../../@types/auth';
+
+const JWT_SECRET = process.env.JWT_SECRET || '';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const signToken = (data: any) => jwt.sign(data, JWT_SECRET);
+
+export const decodeToken = (token: string) => jwt.decode(token);
+
+export const verifyToken = (token: string) => jwt.verify(token, JWT_SECRET);
+
+export const hashStr = async (str: string) => await bcrypt.hash(str, 10);
+
+export const compareStringToHash = async (
+  str: string,
+  hash: string | undefined
+) => {
+  try {
+    if (!hash) return false;
+
+    return await bcrypt.compare(str, hash);
+  } catch (err) {
+    console.error('[COMPAREHASH]', err);
+    return false;
+  }
+};
+
+export const authenticateUser = async (
+  req: TRequestWithUser,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.trim().split(' ')[1];
+  if (!token) {
+    throw new HttpException(403, 'Unauthorized');
+  }
+
+  const decodedUser = verifyToken(token);
+  if (!decodedUser) {
+    throw new HttpException(403, 'Unauthorized');
+  }
+
+  const { id } = decodedUser as TDecodedUser;
+
+  const user = await dbClient.user.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      email: true,
+      username: true,
+    },
+  });
+
+  req.user = user;
+
+  next();
+};
