54b575360d75c55fee5e23e672d4439fc43770fe
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-17 15:40:21 +0100

    feat: error component

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,14 @@
+import { ReactComponent as ERRORSVG } from '../assets/error.svg';
+
+const Error = ({ message, className }) => {
+  return (
+    <div
+      className={`flex items-start gap-3 p-2 border border-gray-300 rounded-md text-base w-52 ${className}`}
+    >
+      <ERRORSVG className="fill-red-600" />
+      {message}
+    </div>
+  );
+};
+
+export default Error;
