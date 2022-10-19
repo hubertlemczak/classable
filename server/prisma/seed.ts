dfc805498b8a4bd4a1c72f62c38171ee425453f1
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-19 21:04:29 +0100

    update db seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,84 +1,150 @@
-// import { hashStr } from '../src/auth';
+import { hashStr } from '../src/auth';
 import dbClient from '../src/utils/dbClient';
 
 async function main() {
-  //   const password = await hashStr('123');
-  //   const user1 = await dbClient.user.create({
-  //     data: {
-  //       email: 'user1@gmail.com',
-  //       password,
-  //     },
-  //   });
-  //   const user2 = await dbClient.user.create({
-  //     data: {
-  //       email: 'user2@gmail.com',
-  //       password,
-  //       following: {
-  //         connect: {
-  //           id: user1.id,
-  //         },
-  //       },
-  //     },
-  //   });
-  //   const user3 = await dbClient.user.create({
-  //     data: {
-  //       email: 'user3@gmail.com',
-  //       password,
-  //       following: {
-  //         connect: {
-  //           id: user1.id,
-  //         },
-  //       },
-  //     },
-  //   });
-  //   await dbClient.chat.create({
-  //     data: {
-  //       name: 'Convo 1',
-  //       chatrooms: {
-  //         createMany: {
-  //           data: [{ userId: user1.id }, { userId: user2.id }],
-  //         },
-  //       },
-  //       messages: {
-  //         createMany: {
-  //           data: [
-  //             {
-  //               content: 'Huss 1',
-  //               userId: user1.id,
-  //             },
-  //             {
-  //               content: 'Huss 2',
-  //               userId: user2.id,
-  //             },
-  //           ],
-  //         },
-  //       },
-  //     },
-  //   });
-  //   await dbClient.conversation.create({
-  //     data: {
-  //       name: 'Convo 2',
-  //       chatrooms: {
-  //         createMany: {
-  //           data: [{ userId: user1.id }, { userId: user3.id }],
-  //         },
-  //       },
-  //       messages: {
-  //         createMany: {
-  //           data: [
-  //             {
-  //               content: 'Huss 1',
-  //               userId: user1.id,
-  //             },
-  //             {
-  //               content: 'Huss 2',
-  //               userId: user3.id,
-  //             },
-  //           ],
-  //         },
-  //       },
-  //     },
-  //   });
+  const password = await hashStr('123');
+
+  const user1 = await dbClient.user.create({
+    data: {
+      email: 'user1@test.com',
+      password,
+      profile: {
+        create: {
+          firstName: 'user1',
+          lastName: 'user1',
+        },
+      },
+    },
+  });
+
+  const user2 = await dbClient.user.create({
+    data: {
+      email: 'user2@test.com',
+      password,
+      profile: {
+        create: {
+          firstName: 'user2',
+          lastName: 'user2',
+        },
+      },
+    },
+  });
+
+  const user3 = await dbClient.user.create({
+    data: {
+      email: 'user3@test.com',
+      password,
+      profile: {
+        create: {
+          firstName: 'user3',
+          lastName: 'user3',
+        },
+      },
+    },
+  });
+
+  const course1 = await dbClient.course.create({
+    data: {
+      name: 'Course 1',
+      category: 'Testing',
+      description: 'Seed generated course',
+    },
+  });
+
+  await dbClient.enrolment.createMany({
+    data: [
+      { role: 'TEACHER', courseId: course1.id, userId: user1.id },
+      { role: 'STUDENT', courseId: course1.id, userId: user2.id },
+      { role: 'STUDENT', courseId: course1.id, userId: user3.id },
+    ],
+  });
+
+  await dbClient.chat.create({
+    data: {
+      name: 'General',
+      chatrooms: {
+        createMany: {
+          data: [
+            { userId: user1.id, courseId: course1.id },
+            { userId: user2.id, courseId: course1.id },
+            { userId: user3.id, courseId: course1.id },
+          ],
+        },
+      },
+      messages: {
+        createMany: {
+          data: [
+            {
+              content: 'Huss 1',
+              userId: user1.id,
+            },
+            {
+              content: 'Huss 2',
+              userId: user2.id,
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  await dbClient.chat.create({
+    data: {
+      name: 'Convo with user 1 & 2',
+      chatrooms: {
+        createMany: {
+          data: [
+            { userId: user1.id, courseId: course1.id },
+            { userId: user2.id, courseId: course1.id },
+          ],
+        },
+      },
+      messages: {
+        createMany: {
+          data: [
+            {
+              content: 'msg 1',
+              userId: user1.id,
+            },
+            {
+              content: 'msg 2',
+              userId: user2.id,
+            },
+            {
+              content: 'msg 3',
+              userId: user1.id,
+            },
+            {
+              content: 'msg 4',
+              userId: user2.id,
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  const ticket1 = await dbClient.chat.create({
+    data: {
+      name: 'Ticket 1',
+      chatrooms: {
+        createMany: {
+          data: [{ userId: user1.id, courseId: course1.id }],
+        },
+      },
+      messages: {
+        createMany: {
+          data: [
+            {
+              content: 'msg 1 pls help with this ticket',
+              userId: user1.id,
+            },
+          ],
+        },
+      },
+      status: 'UNCLAIMED',
+    },
+  });
 }
 
 main()
