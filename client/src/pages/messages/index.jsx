985f33b2cf6889cf5a9fbea247d1018d1dc9b351
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-26 19:59:35 +0100

    cleanup

--- client/src/pages/messages/index.jsx
+++ client/src/pages/messages/index.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ChatRoom from './components/ChatRoom';
 
 import SideBars from './components/SideBars';
