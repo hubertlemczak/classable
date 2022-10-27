ad34c86c960abc87dd8fe8bda95eef26c026800e
Hubert Lemczak <hubertlemczak@gmail.com>
2022-10-27 18:19:47 +0100

    fix: react render error

--- client/src/pages/classroom/components/Video.jsx
+++ client/src/pages/classroom/components/Video.jsx
@@ -1,7 +1,7 @@
 import { AgoraVideoPlayer } from 'agora-rtc-react';
-import { useState } from 'react';
 
 import { ReactComponent as MIC_OFF } from '../../../assets/icons/bxs-microphone-off.svg';
+import RemoteVideo from './RemoteVideo';
 
 export default function Video({
   users,
@@ -34,37 +34,7 @@ export default function Video({
         <AgoraVideoPlayer className="w-full h-full" videoTrack={tracks[1]} />
       </div>
       {users.map(user => {
-        if (user.videoTrack) {
-          const [isFocused, setIsFocused] = useState(false);
-          return (
-            <div
-              className={`h-44 w-72 rounded-md overflow-clip ${
-                isFocused
-                  ? 'fixed top-1/2 -left-1/2 translate-x-1/2 -translate-y-1/2 w-screen h-max scale-[0.85] z-10 '
-                  : 'relative'
-              }`}
-              key={user.uid}
-              onClick={() => setIsFocused(prev => !prev)}
-            >
-              <div className="flex gap-2 absolute bottom-0 left-0 z-10 px-1 bg-gray-700 bg-opacity-70 text-white text-sm rounded-tr-md">
-                {user._audio_muted_ && <MIC_OFF className="w-4 fill-red-500" />}
-                <span>
-                  {user?.firstName} {user?.lastName}
-                </span>
-              </div>
-              <AgoraVideoPlayer
-                className="w-full h-full aspect-video"
-                videoTrack={user.videoTrack}
-              />
-            </div>
-          );
-        } else
-          return (
-            <div
-              className="h-44 w-72 rounded-md overflow-clip bg-black"
-              key={user.uid}
-            ></div>
-          );
+        <RemoteVideo user={user} />;
       })}
     </div>
   );
