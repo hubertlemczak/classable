80b561b6cc3f6a9d23348e03c34ef205b894a8b7
Hubert Lemczak <hubertlemczak@gmail.com>
2022-11-07 14:04:21 +0000

    feat: star change state

---
+++ client/src/pages/resources/components/ResourceItem.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { ReactComponent as UNSTARRED } from '../../../assets/icons/bx-star.svg';
+import { ReactComponent as STARRED } from '../../../assets/icons/bxs-star.svg';
+
+const ResourceItem = ({ id, isStarred, firstName, lastName, path, title }) => {
+  const [starred, setStarred] = useState(isStarred);
+  const navigate = useNavigate();
+
+  const handleStar = async e => {
+    e.stopPropagation();
+    setStarred(prev => !prev);
+  };
+
+  return (
+    <div
+      className=" relative bg-gray-200 h-32 w-48 p-3 rounded-md flex-shrink-0 cursor-pointer group overflow-hidden hover:bg-gray-300"
+      tabIndex={0}
+      key={id}
+      onClick={() => navigate(`./${path}/${id}`)}
+    >
+      <h3 className="font-bold text-xl">{title}</h3>
+      <p className="absolute bottom-2 text-sm text-gray-600 w-36">
+        {name && `${firstName} ${lastName}`}
+      </p>
+      {starred ? (
+        <STARRED
+          className="absolute bottom-2 right-2 w-5 h-5 ml-auto fill-yellow-300 stroke-black hover:scale-110 active:scale-125 translate-x-10 group-hover:-translate-x-0 transition-transform"
+          onClick={e => handleStar(e)}
+        />
+      ) : (
+        <UNSTARRED
+          className="absolute bottom-2 right-2 w-5 h-5 ml-auto hover:scale-110 active:scale-125 translate-x-10 group-hover:-translate-x-0 transition-transform"
+          onClick={e => handleStar(e)}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ResourceItem;
--- client/src/pages/resources/components/ResourceSection.jsx
+++ client/src/pages/resources/components/ResourceSection.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as STARRED } from '../../../assets/icons/bxs-star.svg';
 
 import client from '../../../client';
 import { formatCourseName } from '../../../utils/formatCourseName';
+import ResourceItem from './ResourceItem';
 
 const ResourceSection = ({ resource, title, path, create, name }) => {
   const navigate = useNavigate();
@@ -25,43 +26,22 @@ const ResourceSection = ({ resource, title, path, create, name }) => {
     }
   };
 
-  const handleStar = async (e, isStarred) => {
-    e.stopPropagation();
-
-    if (isStarred) {
-      //
-    } else {
-      //
-    }
-  };
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4 mt-10">{title}</h2>
       <div className="flex gap-4 justify-center flex-wrap sm:justify-start">
         {resource?.map(item => (
-          <div
-            className=" relative bg-gray-200 h-32 w-48 p-3 rounded-md flex-shrink-0 cursor-pointer group overflow-hidden hover:bg-gray-300"
-            tabIndex={0}
+          <ResourceItem
             key={item.id}
-            onClick={() => navigate(`./${path}/${item.id}`)}
-          >
-            <h3 className="font-bold text-xl">{item.title}</h3>
-            <p className="absolute bottom-2 text-sm text-gray-600 w-36">
-              {name &&
-                `${item.user.profile.firstName} ${item.user.profile.lastName}`}
-            </p>
-            {item.isStarred ? (
-              <STARRED
-                className="absolute bottom-2 right-2 fill-yellow-400 ml-auto hover:scale-110 translate-x-10 group-hover:-translate-x-0 transition-transform"
-                onClick={e => handleStar(e, true)}
-              />
-            ) : (
-              <UNSTARRED
-                className="absolute bottom-2 right-2 w-5 h-5 ml-auto hover:scale-110 translate-x-10 group-hover:-translate-x-0 transition-transform"
-                onClick={e => handleStar(e, false)}
-              />
-            )}
-          </div>
+            {...{
+              id: item.id,
+              isStarred: item.isStarred,
+              firstName: item.user.profile.firstName,
+              lastName: item.user.profile.lastName,
+              path,
+              title: item.title,
+            }}
+          />
         ))}
         {create && (
           <button
